c232c8903e4409037b4dd3d3d702e85cdd6835aa
boapara <boapara@dawons.co.kr>
2022-05-24 07:58:31 +0900

    2222

--- nambu/equipment/app/service-worker.js
+++ nambu/equipment/app/service-worker.js
@@ -9,7 +9,7 @@ const { CacheFirst } = strategies;
 core.clientsClaim();
 core.skipWaiting();
 
-const CACHE = "equimentApp-v220524";
+const CACHE = "equimentApp-v220524-1";
 const offlineFallbackPage = "index.html";
 
 
